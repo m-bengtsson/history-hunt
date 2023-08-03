a99743a6f9116c1a6934f541582b654e4dc2d55f
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-08-03 14:03:41 +0200

    dummydata

---
+++ data/userData.js
@@ -0,0 +1,18 @@
+const userData = [
+   {
+      name: 'Matilda',
+      email: 'matilda@hotmail.com',
+      password: 'abc123',
+      friends: [],
+      plannedHunts: [
+         {
+            name: 'First hunt',
+            locations: 'Götaplatsen',
+            friendsInvited: '',
+            estimatedTime: '1h'
+         }
+      ],
+      activeHunts: [],
+      medals: []
+   }
+]
