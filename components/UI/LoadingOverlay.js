18be79d0d8e113e663a0f119b497bb875b34e2d1
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-08-13 17:40:11 +0200

    loading overlay

---
+++ components/UI/LoadingOverlay.js
@@ -0,0 +1,25 @@
+import { StyleSheet, View, Text, ActivityIndicator } from "react-native";
+
+const LoadingOverlay = ({ message }) => {
+   return (
+      <View style={styles.container}>
+         <Text style={styles.message} >{message}</Text>
+         <ActivityIndicator size="small" />
+      </View>
+   )
+};
+
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 40
+   },
+   message: {
+      fontSize: 18,
+      marginBottom: 20,
+   }
+});
+
+export default LoadingOverlay;
